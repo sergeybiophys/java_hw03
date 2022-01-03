1f1096bf97f2b11ee78f4496f1f81239f7f07545
sergeybiophys <rove2hati@gmail.com>
2022-01-03 13:12:38 +0200

    task5 completed

--- src/main/java/org/itstep/task05/Main.java
+++ src/main/java/org/itstep/task05/Main.java
@@ -1,7 +1,24 @@
 package org.itstep.task05;
 
+import java.io.IOException;
+import java.util.Random;
+
 public class Main {
     public static void main(String[] args) {
         // TODO: проверяйте ваш код здесь
+        try{
+            Runner newRunner = new Runner();
+            newRunner.halt();
+        }
+        catch(RuntimeException ex)
+        {
+            System.out.println("**halt**");
+        }
+        catch(IOException ex)
+        {
+            System.out.println(ex.getMessage());
+        }
+
+
     }
 }
